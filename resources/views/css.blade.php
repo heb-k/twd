644d0fdb8460b8873f4fd07615d5e97c3d588877
heb-k <hebe0465@gmail.com>
2023-05-19 22:58:28 -0300

    first commit

---
+++ resources/views/css.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <link href="{{URL:: asset('aet/img/favicon.png')}}" rel="icon">
+  <link href="{{URL:: asset('sets/img/apple-touch-icon.png')}}" rel="apple-touch-icon">
+
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <link href="{{URL:: asset('assets/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+  <link href="{{URL:: asset('assets/vendor/bootstrap-icons/bootstrap-icons.css')}}" rel="stylesheet">
+  <link href="{{URL:: asset('assets/vendor/boxicons/css/boxicons.min.css')}}" rel="stylesheet">
+  <link href="{{URL:: asset('assets/vendor/glightbox/css/glightbox.min.css')}}" rel="stylesheet">
+  <link href="{{URL:: asset('assets/vendor/remixicon/remixicon.css')}}" rel="stylesheet">
+  <link href="{{URL:: asset('assets/vendor/swiper/swiper-bundle.min.css')}}" rel="stylesheet">
+    
+
+  <link href="{{URL:: asset('assets/css/style.css')}}" rel="stylesheet">
+    <title>The Walking Dead</title>
+</head>
+<body>
+  @yield('inicio')
+
+  <script src="{{URL:: asset('assets/vendor/purecounter/purecounter.js')}}"></script>
+  <script src="{{URL:: asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+  <script src="{{URL:: asset('assets/vendor/glightbox/js/glightbox.min.js')}}"></script>
+  <script src="{{URL:: asset('assets/vendor/isotope-layout/isotope.pkgd.min.js')}}"></script>
+  <script src="{{URL:: asset('assets/vendor/swiper/swiper-bundle.min.js')}}"></script>
+  <script src="{{URL:: asset('assets/vendor/waypoints/noframework.waypoints.js')}}"></script>
+  <script src="{{URL:: asset('assets/vendor/php-email-form/validate.js')}}"></script>
+
+  <script src="{{URL:: asset('assets/js/main.js')}}"></script>
+</body>
+</html>
---
+++ resources/views/morto.blade.php
@@ -0,0 +1,252 @@
+@extends ('css')
+@section ('inicio')
+ <header id="header">
+    <div class="container">
+
+      <h1><a href="https://www.starplus.com/pt-br/series/the-walking-dead/6FPLfTcQrTpy?distributionPartner=google">The Walking Dead</a></h1>
+      <h2 style="color: white;">Conheça a <span style="color: white;">série do século!</span></h2>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link active" href="#header">Início</a></li>
+          <li><a class="nav-link" href="#about">Descrição</a></li>
+          <li><a class="nav-link" href="#resume">Temporadas</a></li>
+          <li><a class="nav-link" href="#services">Elenco</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+
+
+    </div>
+  </header>
+
+  <section id="about" class="about">
+
+    <div class="about-me container">
+
+      <div class="section-title">
+        <h2>Descrição</h2>
+        <p>Saiba mais sobre a série:</p>
+      </div>
+
+      <div class="row">
+        <div class="col-lg-4" data-aos="fade-right">
+          <img src="assets/img/rickkk.jpg" class="img-fluid" alt="">
+        </div>
+        <div class="col-lg-8 pt-4 pt-lg-0 content" data-aos="fade-left">
+          <h3>11 temporadas e ainda em lançamento.</h3>
+          <p class="fst-italic">
+            A série está em lançamento desde 2010 e tem previsão para acabar entre 2022 e 2023.
+          </p>
+          <div class="row">
+            <div class="col-lg-6">
+              <ul>
+                <li><i class="bi bi-chevron-right"></i> <strong>Emissora original:</strong> <span>AMC</span></li>
+                <li><i class="bi bi-chevron-right"></i> <strong>Formato:</strong> <span>Série</span></li>
+                <li><i class="bi bi-chevron-right"></i> <strong>Gênero:</strong> <span>Terror, Apocalipse Zumbi</span></li>
+                <li><i class="bi bi-chevron-right"></i> <strong>Criador:</strong> <span>Frank Darabont</span></li>
+              </ul>
+            </div>
+            <div class="col-lg-6">
+              <ul>
+                <li><i class="bi bi-chevron-right"></i> <strong>Baseado em:</strong> <span>The Walking Dead de Robert Kirkman, Tony Moore & Charlie Adlard</span></li>
+                <li><i class="bi bi-chevron-right"></i> <strong>Transmissão:</strong> <span>31 de outubro de 2010 – presente</span></li>
+                <li><i class="bi bi-chevron-right"></i> <strong>Idioma original:</strong> <span>Inglês</span></li>
+                <li><i class="bi bi-chevron-right"></i> <strong>Musica composta por:</strong> <span>	Bear McCreary</span></li>
+              </ul>
+            </div>
+          </div>
+          <p>
+          Em The Walking Dead, depois de ser baleado e passar meses em coma, o xerife Rick Grimes (Andrew Lincolm) acorda sozinho em um hospital deserto. Para sua surpresa, o mundo como conhecia deixou de existir depois que um perigoso vírus provoca uma infestação de zumbis na cidade de Cynthiana, em Kentucky, nos Estados Unidos. Desesperado, Rick enfrenta a estranha ameaça na expectativa de reencontrar a mulher Lori (Sarah Wayne Callies) e o filho Cal (Chandler Riggs). Ele decide unir-se aos homens e mulheres sobreviventes para que tenham mais força para combater o fenômeno que os atinge. O grupo percorre diferentes lugares em busca de soluções para o problema. Aos poucos, eles percebem que a infestação de mortos é maior do que pensavam, se espalhando por todo o país, e possivelmente o resto do mundo. A sociedade que conheciam já não existe mais e tudo que resta é o desejo de sobreviver. Em meio ao caos da nova ordem mundial, Rick e seus companheiros percebem que os humanos sobreviventes podem ser uma ameaça maior que os próprios zumbis.
+          </p>
+        </div>
+      </div>
+
+    </div>
+
+    <div class="counts container">
+
+      <div class="row">
+
+        <div class="col-lg-3 col-md-6">
+          <div class="count-box">
+            <span data-purecounter-start="0" data-purecounter-end="11" data-purecounter-duration="1" class="purecounter"></span>
+            <p>Temporadas</p>
+          </div>
+        </div>
+
+        <div class="col-lg-3 col-md-6 mt-5 mt-md-0">
+          <div class="count-box">
+            <span data-purecounter-start="0" data-purecounter-end="162" data-purecounter-duration="1" class="purecounter"></span>
+            <p>Episódios</p>
+          </div>
+        </div>
+
+        <div class="col-lg-3 col-md-6 mt-5 mt-lg-0">
+          <div class="count-box">
+            <span >42~67</span>
+            <p>Duração dos episódios (min)</p>
+          </div>
+        </div>
+
+        <div class="col-lg-3 col-md-6 mt-5 mt-lg-0">
+          <div class="count-box">
+            <span>155 (70)</span>
+            <p>Indicações (Vitórias)</p>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+
+  </section>
+
+  <section id="resume" class="resume">
+    <div class="container">
+
+      <div class="section-title">
+        <h2>Temporadas</h2>
+        <p>Resumo das temporadas</p>
+      </div>
+
+      <div class="row">
+        <div class="col-lg-8">
+          <h3 class="resume-title">1ª temporada</h3>
+          <div class="resume-item pb-0">
+            <h4>2010</h4>
+            <p><em>Rick Grimes é o xerife de uma pequena cidade do estado da Georgia, quando certo dia, é baleado por criminosos durante uma perseguição e entra em coma. Semanas depois, ele acorda em um hospital abandonado e totalmente danificado. Ao sair do hospital, Rick se encontra em um mundo pós-apocalíptico dominado por mortos-vivos. Depois de conhecer Morgan Jones e seu filho, Duane, que lhe explica o novo mundo, Rick decide ir para Atlanta atrás de sua família, onde um possível centro de refugiados foi montado pela Guarda Nacional. Ao chegar em Atlanta, ele logo descobre que a cidade está vazia e foi dominada pelos mortos.</em></p>
+          </div>
+
+          <h3 class="resume-title">2ª temporada</h3>
+          <div class="resume-item">
+            <h4>2011~2012</h4>
+            <p><em>Rick reencontrou sua família bem e segura com um grupo de sobreviventes. Porém, agora eles são obrigados a ir embora de Atlanta após perderem as esperanças de serem salvos e partem em busca de um novo lugar onde possam permanecer seguros e livres da horda de zumbis espalhadas pelo mundo. Quando encontram uma fazenda que permanece isolada da ameaça zumbi, o grupo tenta conquistar o seu lugar ali, dividindo o espaço com os donos da fazenda. Em meio ao caos, alguns dos sobreviventes vão revelando faces até então ocultas, e o grupo percebe que os zumbis não são o único perigo que existe.</p>
+          </div>
+        </div>
+
+        <div class="col-lg-8">
+          <h3 class="resume-title">3ª temporada</h3>
+          <div class="resume-item">
+            <h4>2012~2013</h4>
+            <p><em>Após os zumbis invadiram a fazenda, os sobreviventes peregrinam por alguns meses procurando por outros abrigos, até que encontram proteção atrás das grades de uma prisão abandonada. Porém a paz conquistada não dura muito tempo, pois o novo abrigo desperta a ira do primeiro grande vilão da série: o Governador, sádico líder de uma comunidade não muito longe dali chamada Woodbury. Agora o grupo de Rick deverá lutar para defender seu novo lar e suas vidas.</em></p>
+          </div>
+          <h3 class="resume-title">4ª temporada</h3>
+          <div class="resume-item">
+            <h4>2013~2014</h4>
+            <p><em>Superando as recentes perdas, os sobreviventes liderados por Rick Grimes continuam com suas vidas na prisão, agora reforçada com a presença dos antigos moradores de Woodbury. No entanto, a paz é novamente deixada de lado quando uma estranha epidemia começa a se espalhar pela prisão, ameaçando a vida dos sobreviventes. Além disso, o grupo de Rick também tem que lidar com o retorno do Governador, agora com um novo exército e disposto a tomar o controle da prisão de uma vez por todas.</em></p>
+          </div>
+          </div>
+
+          <div class="col-lg-8">
+          <h3 class="resume-title">5ª temporada</h3>
+          <div class="resume-item pb-0">
+            <h4>2014~2015</h4>
+            <p><em>A prisão foi destruída e vidas foram perdidas. Os sobreviventes, agora espalhados, se reencontram em um possível novo refúgio chamado Terminus. No entanto, nem tudo é o que aparenta ser e o grupo de Rick descobre que seus habitantes são canibais e novamente precisam lutar por suas vidas. Após se livrarem dos canibais, o grupo precisa comandar duas missões: resgatar Beth Greene de um hospital em Atlanta, onde está aprisionada, e levar Eugene Porter até Washington para possivelmente conseguir a cura da epidemia zumbi. Quando ambas missões falham, o grupo acaba ficando sem rumo novamente, até que conhecem um estranho disposto a levá-los para um comunidade chamada Alexandria, localizada nos arredores da capital.</em></p>
+          </div>
+
+          <h3 class="resume-title">6ª temporada</h3>
+          <div class="resume-item">
+            <h4>2015~2016 </h4>
+            <p><em>Após serem aceitos em Alexandria, Rick Grimes e seu grupo tentam se adaptar a nova vida, lidando com os recentes acontecimentos. Mas a paz não é duradoura. Dessa vez terão que enfrentar ameaça atrás de ameaça, seja dentro, com os moradores mostrando algumas de suas reais personalidades, quanto fora da comunidade quando uma manada de zumbis se aproxima dos portões, além de enfrentarem um grupo de saqueadores psicopatas chamados de Lobos e o insano grupo dos Salvadores, mas, dessa vez, os sobreviventes vão descobrir que os Salvadores e seu líder, Negan, não são um grupo qualquer.</p>
+          </div>
+        </div>
+
+        <div class="col-lg-8">
+          <h3 class="resume-title">7ª temporada</h3>
+          <div class="resume-item pb-0">
+            <h4>2016~2017</h4>
+            <p><em>Membros do grupo de Rick são mortos brutalmente por Negan, que exige que eles lhe forneçam metade dos suprimentos de Alexandria. Despedaçado, Rick obedece suas ordens; porém, mais tarde, é convencido a lutar quando é apresentado a algumas outras comunidades, como Hilltop, o Reino, Oceanside e os Catadores. Agora, Rick fará o possível para unir todas as comunidades e colocar de vez um fim no reinado de Negan e os Salvadores.</em></p>
+          </div>
+
+          <h3 class="resume-title">8ª temporada</h3>
+          <div class="resume-item">
+            <h4>2017~2018</h4>
+            <p><em>A guerra está declarada. As comunidades estão unidas e otimistas para derrotar os Salvadores. Vários Salvadores são mortos e presos. No entanto, a atitude de membros do grupo de Rick em busca de vingança acaba dando errado, o que dá oportunidade aos Salvadores de se reerguerem e novamente tomarem o controle das comunidades. Agora, cabe a Rick e aos outros arquitetarem um novo plano para saírem salvos e vitoriosos, dando um novo desfecho a esta guerra.</p>
+          </div>
+        </div>
+
+        <div class="col-lg-8">
+          <h3 class="resume-title">9ª temporada</h3>
+          <div class="resume-item pb-0">
+            <h4>2018~2019</h4>
+            <p><em>Após a guerra contra os Salvadores, que causou a morte de várias pessoas, afetando consideravelmente os sobreviventes, Rick decide acreditar na esperança de seu filho, Carl, e tentar construir um futuro entre as comunidades; então, sob sua liderança, os sobreviventes trabalham na reconstrução da civilização. As comunidades enfrentam obstáculos e perigos inesperados, um deles sendo uma força formidável que se veste e vive como os zumbis: os Sussurradores, um perigoso grupo que ameaça toda a ideia de civilização que todos trabalharam para construir.</em></p>
+          </div>
+
+          <h3 class="resume-title">10ª temporada</h3>
+          <div class="resume-item">
+            <h4>2019~2021</h4>
+            <p><em>Poucos meses depois, quando o grupo de sobreviventes cruzou o caminho dos Sussurradores no duro inverno, as comunidades ainda estão tentando lidar com as consequências do encontro com o horrível poder de Alpha e os Sussurradores. Os selvagens e perigosos Sussurradores são uma ameaça que os sobreviventes nunca enfrentaram e precisarão ter cautela para poder enfrentá-los ou pelo menos dividir território com eles. A ideia de que uma civilização pode sobreviver num mundo cheio de mortos-vivos é colocada em dúvida diante de um novo cenário perigoso.</p>
+          </div>
+        </div>
+
+        <div class="col-lg-8">
+          <h3 class="resume-title">11ª temporada</h3>
+          <div class="resume-item pb-0">
+            <h4>2021~Atualmente</h4>
+            <p><em>A décima primeira e última temporada de The Walking Dead, uma série de televisão pós-apocalíptica de terror produzida pela AMC que estreou nos Estados Unidos em 22 de agosto de 2021 e consistirá em 24 episódios.[1][2] No Brasil, de acordo com a Disney, a última temporada é exclusiva do novo serviço de streaming da empresa, o Star+.[3] Desenvolvida para a televisão por Frank Darabont, a série é baseada na série homônima de histórias em quadrinhos de Robert Kirkman, Tony Moore e Charlie Adlard. Os produtores executivos são Kirkman, David Alpert, Scott M. Gimple, Angela Kang, Greg Nicotero, Joseph Incaprera, Denise Huth e Gale Anne Hurd, com Kang como showrunner para sua terceira e última temporada. A décima primeira temporada recebeu até agora críticas positivas por parte dos críticos.</em></p>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </section>
+
+  <section id="services" class="services">
+    <div class="container">
+
+      <div class="section-title">
+        <h2>Elenco</h2>
+        <p>Personagens principais</p>
+      </div>
+
+      <div class="row">
+        <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
+          <div class="icon-box">
+          <img src="assets/img/rick.jpg" class="img-fluid" alt="">
+            <h4>Rick Grimes</h4>
+          </div>
+        </div>
+
+        <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0">
+          <div class="icon-box">
+          <img src="assets/img/daryl.jpg" class="img-fluid" alt="">
+            <h4>Daryl Dixon</h4>
+          </div>
+        </div>
+
+        <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-lg-0">
+          <div class="icon-box">
+          <img src="assets/img/shane.jpg" class="img-fluid" alt="">
+            <h4><a href="">Shane Walsh</a></h4>
+          </div>
+        </div>
+
+        <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
+          <div class="icon-box">
+            <img src="assets/img/glenn.jpg" class="img-fluid" alt=""> 
+            <h4><a href="">Glenn Rhee</a></h4>
+          </div>
+        </div>
+
+        <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
+          <div class="icon-box">
+          <img src="assets/img/michonne.jpg" class="img-fluid" alt=""> 
+            <h4><a href="">Michonne</a></h4>
+          </div>
+        </div>
+
+        <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
+          <div class="icon-box">
+          <img src="assets/img/hershel.jpg" class="img-fluid" alt=""> 
+            <h4><a href="">Hershel Greene</a></h4>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+  </section>
+
+  @endsection
